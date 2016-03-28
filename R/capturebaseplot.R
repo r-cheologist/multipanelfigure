9b82d1c4d7ad96f2f00507627c5516894456a605
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2016-03-28 07:29:42 +0300

    added missing imports

--- R/capturebaseplot.R
+++ R/capturebaseplot.R
@@ -3,7 +3,7 @@
 #' Capture a plot drawn using base graphics as a grid \code{grob}.
 #' @param expr A expression that draws a plot using base graphics.
 #' @return An object of class \code{gTree}.
-#' @note A side effect of this function is that plots gt drawn twice: once as
+#' @note A side effect of this function is that plots get drawn twice: once as
 #' a base plot, and secondly as a grid plot.
 #' @examples
 #' p <- capturebaseplot(hist(rnorm(1000), seq(-4, 4, 0.2)))
@@ -15,6 +15,8 @@
 #'   title("One to five")
 #' })
 #' grid::grid.draw(p2)
+#' @importFrom gridGraphics grid.echo
+#' @importFrom grid grid.grab
 #' @export
 capturebaseplot <- function(expr)
 {
